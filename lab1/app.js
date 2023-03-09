bdf4a9a1724a41552c65db56ad9a08ba512fae18
quangvu2308 <vunppd08017@fpt.edu.vn>
2023-03-09 14:35:51 +0700

    nopla1

---
+++ lab1/app.js
@@ -0,0 +1,16 @@
+// Nhap a va b
+var a = parseInt(prompt("Nhap so nguyen a:"));
+var b = parseInt(prompt("Nhap so nguyen b:"));
+var c = a + b;
+
+// in ra ket qua bang 2 cach document.write và console.log
+document.write("Tong cua " + a + " va " + b + " la " + c + "<br>");
+console.log("Tong cua " + a + " va " + b + " la " + c);
+
+// Nhap d
+var d = parseInt(prompt("Nhap so nguyen d:"));
+var result = c + d;
+
+// in ra ket qua bang 2 cach alert và console.log
+alert("Tong cua " + c + " va " + d + " la " + result);
+console.log("Tong cua " + c + " va " + d + " la " + result);
